1399e5d1c74365d37b0f0be64d1aba1db98b826a
juanes <67403119+juanesGarcia@users.noreply.github.com>
2024-05-17 19:46:36 -0500

    acomodando cors origin y validation name 2

--- src/validators/users.js
+++ src/validators/users.js
@@ -14,7 +14,7 @@ const password= check('password')
 
   const name= check('name')
   .isLength({ min: 3 ,max:20})
-  .withMessage('el nombre debe tener al menos 3 caracteres y maximo 24')
+  .withMessage('el nombre debe tener al menos 3 caracteres y maximo 20')
   .matches(/^[^\s]+$/, 'i')
   .withMessage('El nombre no debe contener espacios en blanco.');
 
