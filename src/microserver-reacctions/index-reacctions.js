f93be232dca2f35fcaf91d882c2ea2d36815cc09
juanes <67403119+juanesGarcia@users.noreply.github.com>
2024-06-23 23:59:07 -0500

    agregando el index.js en todos mi microservicios

---
+++ src/microserver-reacctions/index-reacctions.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+const cors = require('cors')
+
+PORT=3003;
+app.use(morgan('dev'))
+//initialize middlewares
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(cors({
+    origin: true,
+    credentials: true
+  }));
+ /* const corsOptions = {
+    origin: 'https://tattopro.com',
+    credentials: true,
+  };
+  app.use(cors(corsOptions));*/
+
+//import routes 
+const authRoutes=require('./network');
+
+
+
+const appStart =()=>{
+    try {
+        app.listen(PORT,()=>{
+            console.log(`listener micro-reacctions: ${PORT}`);
+        })
+        
+    } catch (error) {
+        console.log(`Error:${error.message}`);
+    }
+}
+
+appStart()
